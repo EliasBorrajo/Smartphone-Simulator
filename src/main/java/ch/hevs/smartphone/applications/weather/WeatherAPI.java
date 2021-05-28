b1a18a90eb250b35bcf9e2276cd37e8fe061be0f
Elias <borrajo.elias@gmail.com>
2021-05-28 13:26:11 +0200

    Reprise du code de milena, et ajout de l'API

---
+++ src/main/java/ch/hevs/smartphone/applications/weather/WeatherAPI.java
@@ -0,0 +1,20 @@
+package ch.hevs.smartphone.applications.weather;
+
+// Les deux imports de google sont nécessaires pour la methode "jsonToMap"
+import com.google.gson.*;
+import com.google.gson.reflect.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class WeatherAPI
+{
+
+}
--- src/main/java/ch/hevs/smartphone/applications/weather/WeatherGUI.java
+++ src/main/java/ch/hevs/smartphone/applications/weather/WeatherGUI.java
@@ -3,8 +3,10 @@ package ch.hevs.smartphone.applications.weather;
 import javax.swing.*;
 import java.awt.*;
 
-public class WeatherGUI extends JPanel {
+public class WeatherGUI extends JPanel
+{
     public WeatherGUI(){
         this.setBackground(Color.RED);
     }
+
 }
