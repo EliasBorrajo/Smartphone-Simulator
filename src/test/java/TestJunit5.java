532df8fd269c157653efda19249003e5c1bcb0e3
Elias <borrajo.elias@gmail.com>
2021-06-04 13:32:10 +0200

    Elias, Ajout du JUNT testing

---
+++ src/test/java/TestJunit5.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+/**
+ * Le testing permet, en cas de MAJ de Java, de vérifer rapidement que tout le projet fonctionne,
+ * et si ce n'est pas le cas, de voir quelle partie ne fonctionne plus.
+ *
+ * Le test devra fournir un resultat, qui doit être PAREILLE que le résultat attendu.
+ *
+ * Tester la partie fonctionelle NON_Graphique
+ * - Serialisation
+ * - Tester l'API APP, Si il arrive à se connecter, récuperer les data correctement.
+ * - Gallery totalement UI, donc
+ */
+public class TestJunit5
+{
+    @Test
+    void testApp()
+    {
+    }
+}
