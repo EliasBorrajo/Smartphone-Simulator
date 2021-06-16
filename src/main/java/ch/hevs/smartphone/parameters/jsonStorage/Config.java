65fb9e270d98df37f47d2cd179b603a06279fd8b
Jonathan <jonathan.bourquin@students.hevs.ch>
2021-06-16 20:05:54 +0200

    environnement variable changed : HOME

--- src/main/java/ch/hevs/smartphone/parameters/jsonStorage/Config.java
+++ src/main/java/ch/hevs/smartphone/parameters/jsonStorage/Config.java
@@ -28,7 +28,7 @@ public class Config {
                                                     // All personal files in JSON format will be stored in this folder
     private String configFilePath;
     private String storePath;                       // Is the value contained in the environment variable. We return it to the getters.
-    private static final String VARIABLE_ENVIRONNEMENT = "SMARTPHONEPROJECT";
+    private static final String VARIABLE_ENVIRONNEMENT = "HOME";
     // Contains the NAME of the ENVIRONMENT VARIABLE.
     // The method: "System.getenv (VARIABLE_ENVIRONNEMENT)" will know WHICH VALUE of VARIABLE to look for.
 
